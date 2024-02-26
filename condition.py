2ead8457e0fb405326e5846ef9987ea6bd7a214d
shahdev kumar <shahdevkumar.d@gmail.com>
2024-02-26 13:15:38 +0500

    python programming

---
+++ condition.py
@@ -0,0 +1,6 @@
+a=33
+b=200
+if b>a:
+print("b is greter than a")
+#An indentation error is a type of syntax error that occurs in programming languages such as Python, where the indentation (spacing at the beginning of lines) is significant for defining the structure of the code. In Python, indentation is used to indicate blocks of code, such as those within loops, conditionals, or function definitions.
+    
---
+++ test.py
@@ -0,0 +1,2 @@
+print("Hello world.....");
+print("It is my first program in Python.....")
